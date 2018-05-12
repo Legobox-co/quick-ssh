7b3fe54da8bbb84b548ce8088620bec1dd579ee1
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2018-05-12 12:01:17 +0100

    base struct

--- src/SshService.php
+++ src/SshService.php
@@ -1,5 +1,25 @@
 <?php
 
+namespace Legobox\QuickSsh;
+
+use Illuminate\Contracts\Container\Container;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+/**
+ * Running all ssh services, Creati
+ */
 class SshService {
-	
+
+	/**
+	 * Illuminate App instance
+	 *
+	 * @var [Illuminate\Contracts\Container\Container]
+	 */
+	public $app;
+
+	public function __construct(Container $app){
+		$this->app = $app;
+	}
+
+
 }

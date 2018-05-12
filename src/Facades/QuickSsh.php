692bcf9770325b3ce487997dacf79541b947b7fc
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2018-05-12 12:12:48 +0100

    readme

---
+++ src/Facades/QuickSsh.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Legobox\QuickSsh\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class QuickSsh extends Facade {
+
+	/**
+	 * Setting up the facade assessor
+	 *
+	 * @return void
+	 */
+	protected static function getFacadeAccessor(){
+        return 'quickssh';
+    }
+}
